62a12f3ead947192586bcc9cd8004495f79a7a00
anozie5 <chinemeremanozie5@gmail.com>
2024-08-10 06:28:43 +0100

    still here

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -95,8 +95,8 @@ from datetime import timedelta
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes = 120),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': False,
+    'ROTATE_REFRESH_TOKENS': True,    
+    'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
 
     'ALGORITHM': 'HS256',
